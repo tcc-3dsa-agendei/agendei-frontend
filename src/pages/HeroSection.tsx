b7f4680bae9b38bf56f943c158b0a44bbad46f2e
Nelson Francs <nelsonfrancisco547@gmail.com>
2026-06-23 21:25:41 -0300

    Criação da página de direcionamento.
    Criação da primeira primeira tela que o usuário terá contato; Criação da pasta de conteúdos "assets" e atualização das rotas.

---
+++ src/pages/HeroSection.tsx
@@ -0,0 +1,42 @@
+import initialStyle from "./HeroSection.module.css"
+import { Link } from "react-router-dom";
+import form from "../assets/form.png";
+
+export function HeroSection() {
+  return (
+    <div className={initialStyle.main}>
+      <div className={initialStyle.box}>
+        <div className={initialStyle.left}>
+          <img src={form} id={initialStyle.leftPng}/>
+          <div className={initialStyle.center}>
+            <p className={initialStyle.title}>Agendei.com</p>
+            <p className={initialStyle.subtitle}>
+              Agendar não precisa ser complicado
+            </p>
+          </div>
+          <div id={initialStyle.lineLt}></div>
+          {/*Linha verde, no canto inferior esquerdo*/}
+        </div>
+
+        <div className={initialStyle.right}>
+          <div id={initialStyle.lineRt}></div>
+          {/*Linha verde, no canto superior direito*/}
+          <div className={initialStyle.center}>
+            <div className={initialStyle.text}>
+              <p id={initialStyle.subtitle1}>Seja bem-vindo(a)</p>
+              <p id={initialStyle.subtitle2}>
+                ao seu site de agendamentos totalmente gratuito!
+              </p>
+            </div>
+            <Link to="login" className={initialStyle.button}>
+              Começar
+            </Link>
+          </div>
+          <img src={form} id={initialStyle.rightPng}/>
+        </div>
+      </div>
+      <div className={initialStyle.footer}></div>{" "}
+      {/*Footer genérico para seguir o modelo*/}
+    </div>
+  );
+}
--- src/routes/Routes.tsx
+++ src/routes/Routes.tsx
@@ -1,9 +1,12 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 //Coloque os importes das páginas conforme vocês forem adicionando/criando
 //EX:
-import { Login } from "../pages/Login"
+//import { Login } from "../pages/Login"
 import { Home } from "../pages/Home"
+import { Login } from "../pages/Login";
 import { Register } from "../pages/Register"
+import { HeroSection } from "../pages/HeroSection";
+
 
 
 export function SetupNavigation() {
@@ -11,15 +14,11 @@ export function SetupNavigation() {
         <BrowserRouter>
             <Routes>
 
-                <Route path='/' element={ <Login/>} />
-
-                <Route path='Home' element={ <Home/> }>
-                    {/*Colocar todas as páginas que possuem o menu
-                    EX:*/}
-                    {/*<Route path='Profile' element={ <Profile/> }/>*/}
-                    <Route path='Register' element={ <Register/> }/>
-
-                </Route>
+                <Route path='/' element={ <HeroSection/>} />
+                <Route path='register' element={ <Register/>} />
+                <Route path='login' element={ <Login/>} />
+                <Route path='register' element={ <Register/>} />
+                <Route path='home' element={ <Home/> }/>
                 
 
             </Routes>
