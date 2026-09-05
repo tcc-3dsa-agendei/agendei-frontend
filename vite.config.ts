dd4de6ddf270dba6a225f55032c7eeb106dc9ec8
devvitor67 <desenvolvedorvitor67@gmail.com>
2026-09-05 20:22:12 -0300

    feat: adiciona alias de importação @/*

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,8 @@ import react, { reactCompilerPreset } from "@vitejs/plugin-react"
 import { defineConfig } from "vite"
 
 export default defineConfig({
-	plugins: [react(), babel({ presets: [reactCompilerPreset()] })]
+	plugins: [react(), babel({ presets: [reactCompilerPreset()] })],
+	resolve: {
+		tsconfigPaths: true
+	}
 })
